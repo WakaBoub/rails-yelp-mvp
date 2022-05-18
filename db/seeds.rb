fb1bfda2a38288efc5b56f00f737cd07af77c9d0
lidwine diot <lidwine.diot@gmail.com>
2022-05-18 21:58:11 +0200

    YELP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: 'Chez Lulu', address: 'rue du bon resto', phone_number: '01 02 03 04 05', category: 'french')
+Restaurant.create(name: 'Chez Roro', address: 'rue Rouge', phone_number: '01 02 03 04 06', category: 'chinese')
+Restaurant.create(name: 'Italiano', address: 'rue Aubert', phone_number: '01 02 03 04 07', category: 'italian')
+Restaurant.create(name: 'Le Phare', address: 'rue Albert Petit', phone_number: '01 02 03 04 08', category: 'french')
+Restaurant.create(name: 'Chez LoupGarou', address: 'avenue du bois', phone_number: '01 02 03 04 09', category: 'japanese')
+Restaurant.create(name: 'Aventure', address: 'chemin vert', phone_number: '01 02 03 12 05', category: 'belgian')
